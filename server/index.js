6f36825642443433624fa374210daed61b56ed55
wlgus95799@naver.com <wlgus95799@naver.com>
2024-07-25 17:17:08 +0900

    추가

--- server/index.js
+++ server/index.js
@@ -8,16 +8,24 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 
-const users= [{
+const users= [
+  {
   idx : 1,
   id: "test",
   pw: "1234",
   created: new Date("2024-07-20"),
   email:"test@naver.com",
   nick: "겁나 무서운 전사"
-
-  
-}];
+},
+{
+  idx : 2,
+  id: "hello",
+  pw: "hello1234",
+  created: new Date("2024-07-22"),
+  email:"helloworld@naver.com",
+  nick: "헬로월드"
+}
+];
 
 
 
@@ -29,10 +37,30 @@ app.get('/main', function (req, res) {
 });
 
 app.post('/login', function(req, res) {
-  let{user_id, user_pw}=req.body;
-  console.log(req.body);
-  console.log("아이디: "+ req.body.user_id);
-  console.log("비밀번호: "+ req.body.user_pw);
+  let {user_id, user_pw} = req.body;
+  
+  let foundUser = users.filter(user => user.id == user_id && user.pw == user_pw);
+
+  console.log(foundUser)
+
+
+  if(user_id== "test" && user_pw=="1234"){
+    res.json({
+    message: "login ok",
+    status: 200,
+    isLogin: true,
+    user: foundUser.nick,
+    redirect_url : "/home"
+  });
+}  else {
+  res.json({
+    message: "login fail",
+    status: 400,
+    isLogin: false,
+    user: null,
+    redirect_url: null
+  });
+}
 })
 app.get('/chart', function(req, res) {
   res.json([
@@ -41,11 +69,11 @@ app.get('/chart', function(req, res) {
       title: '데드풀과 울버린',
       satisfy: 99,
       ratio: 44.4,
-      img: ['images/poster1.jpg'] ,
+      img: ['images/poster1.jpg','images/poster2.jpg'] ,
       url: '/movie?cate=action&sf?code={ranking}'
     },
     {
-      ranking:1,
+      ranking:2,
       title:'파일럿',
       satisfy: 99,
       ratio: 15.7,
